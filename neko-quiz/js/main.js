783f189c812ab0972e77dedd811ab0e3b11243df
suzuka92 <suzukasato.job@gmail.com>
2020-03-01 00:14:29 +0900

    機能追加 ・クイズ選択肢をランダムに表示 ・クイズの正誤判定

--- neko-quiz/js/main.js
+++ neko-quiz/js/main.js
@@ -6,49 +6,92 @@
     const btn = document.getElementById('btn');
 
     const quizSet = [
-        {q: 'What????' , c: ['aaaaa', 'bbbbb' ,'ccccc' , 'dddd']},
-        {q: 'What????' , c: ['aaaaa', 'bbbbb' ,'ccccc' , 'dddd']},
-        {q: 'What????' , c: ['aaaaa', 'bbbbb' ,'ccccc' , 'dddd']},
-        {q: 'What????' , c: ['aaaaa', 'bbbbb' ,'ccccc' , 'dddd']},
-        {q: 'What????' , c: ['aaaaa', 'bbbbb' ,'ccccc' , 'dddd']},
-    ]
+        {q: 'ねこが食べちゃいけないものは？' , c: ['たまねぎ', 'ささみ' ,'きゃべつ' , '生卵']},
+        {q: 'ねこがふみふみしているときの気持ちで誤っているのは？' , c: ['むかつくわ〜', 'かまって〜' ,'ねむたい' , '落ち着くわ〜']},
+        {q: 'ねこがすりすりするときの気持ちは？' , c: ['においつけとこ', 'いいにおいで癒される〜','はやくあそぼ〜' ]},
+        {q: 'ねこがしっぽを振るときの気持ちは？' , c: ['むかつくわ〜', '興奮してきた！' ,'ワクワク！' , 'お腹すいた']},
+        {q: 'ねこがゴロンしてお腹を見せるときの気持ちは？' , c: ['こっち見て！', 'つかれた！' ,'お腹すいた！' , 'ねむい！']},
+        {q: 'ねこがゴロンしてお腹を見せるときの気持ちは１？' , c: ['こっち見て！', 'つかれた！' ,'お腹すいた！' , 'ねむい！']},
+        {q: 'ねこがゴロンしてお腹を見せるときの気持ちは２？' , c: ['こっち見て！', 'つかれた！' ,'お腹すいた！' , 'ねむい！']},
+        {q: 'ねこがゴロンしてお腹を見せるときの気持ちは３？' , c: ['こっち見て！', 'つかれた！' ,'お腹すいた！' , 'ねむい！']},
+        {q: 'ねこがゴロンしてお腹を見せるときの気持ちは４？' , c: ['こっち見て！', 'つかれた！' ,'お腹すいた！' , 'ねむい！']},
+        {q: 'ねこがゴロンしてお腹を見せるときの気持ちは５？' , c: ['こっち見て！', 'つかれた！' ,'お腹すいた！' , 'ねむい！']},
 
-    //正誤判定する
+    ];
+
+    
     let currentNum = 0;
-    function checkAnswer() {
-        if(li.textContent === quizSet[currentNum].c[0]) {
-            console.log('あたり！');
-        }else{
-            console.log('シャーーーーー！！！');
+    let isAnswerd; 
+
+        function shuffle(arr) {
+
+            //配列全体の中からランダムな要素を選ぶ           
+            for (let i = arr.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() *  (i + 1));
+                [arr[j],arr[i]]=[arr[i],arr[j]];
+            }
+            //最後の要素と入れ替える
+            return arr;
         }
-}
+    
+    function checkAnswer(li) {
+        if (isAnswerd === true) {
+            return;
+        }
+        
+
+        if(li.textContent === quizSet[currentNum].c[0]) {
+            li.classList.add('correct');
+            btn.classList.remove('disabled');
+        } else {
+            li.classList.add('wrong');
+        }
+        
+    }
 
     function setQuiz() {
-        //問題文を表示
+        isAnswerd = false;
         question.textContent = quizSet[currentNum].q;
 
-        //選択肢を小要素liを作って表示
-        quizSet[currentNum].c.forEach(choice => {
+        while (choices.firstChild){
+            choices.removeChild(choices.firstChild);
+        }
+
+        const shuffledChoices = shuffle([...quizSet[currentNum].c]);
+        shuffledChoices.forEach(choice => {
             const li = document.createElement('li');
             li.textContent = choice;
-            li.addEventListener('click', () =>{
+            li.addEventListener('click' , () => {
                 checkAnswer(li);
-        });
-        choices.appendChild(li);
-    });
 
-}
+            });
+            choices.appendChild(li);
+        });
+
+
+        if (currentNum === quizSet.length -1) {
+        btn.textContent = '成績をみる';
+
+    }
+
+    }
+
 
 setQuiz();
 
+btn.addEventListener('click' , () => {
+    if(btn.classList.contains('disabled')){
 
+        return;
+    }
+    btn.classList.add('disabled');
+    currentNum++;
+    setQuiz();
 
+});
 
-//次へボタンを押すと次の問題を表示
-//正答数結果を表示する
+let score =0 ;
 
 
 
 }
-
-//
