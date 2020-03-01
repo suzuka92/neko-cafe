c6850a8e11465e155df9607dddf3fbaf1b8e7a8e
suzuka92 <suzukasato.job@gmail.com>
2020-03-01 13:57:42 +0900

    Revert "機能追加"
    This reverts commit 5ad915554b77432634c4c4b4f243a40a3aa6bd20.

--- neko-quiz/js/main.js
+++ neko-quiz/js/main.js
@@ -4,27 +4,24 @@
     const question = document.getElementById('question');
     const choices = document.getElementById('choices');
     const btn = document.getElementById('btn');
-    const result = document.getElementById('result');
-    const scoreLabel = document.querySelector('#result > h1');
-    const scoreText = document.querySelector('#result > p');
 
-    const quizSet = shuffle([
+    const quizSet = [
         {q: 'ねこが食べちゃいけないものは？' , c: ['たまねぎ', 'ささみ' ,'きゃべつ' , '生卵']},
-        {q: 'ねこが嫌がらないことは？' , c: ['あごの下なでること', '濡れること' ,'初めましての人' , 'トイレが汚いこと']},
-        {q: 'ねこがふみふみしているときの気持ちで間違っているのは？' , c: ['むかつく〜', 'かまって〜' ,'ねむい〜' , '落ち着く〜']},
-        {q: 'ねこがすりすりするときの気持ちは？' , c: ['においつけとこ', 'いいにおいするやん','はやくあそぼ','かゆい' ]},
-        {q: 'ねこがしっぽをぶんぶん振るときの気持ちは？' , c: ['いらいら', '興奮してきた！' ,'たのしい' , 'お腹すいた']},
-        {q: 'ねこがゴロンしてお腹を見せるときの気持ちは？' , c: ['こっち見て！', 'つかれた！' ,'お腹すいた！' , 'ねむい']},
-        {q: 'ねこが耳を伏せたときの気持ちは？' , c: ['こわい', 'うれしい' ,'いらいら' , 'びっくり']},
-        {q: 'ねこがゆっくり瞬きしているときの気持ちは？' , c: ['だいすき', 'ねむい' ,'きらい' , 'いらいら']},
-        {q: 'ねこ「ア〜オ〜」と鳴くときの気持ちは？' , c: ['ムラムラする', 'かまって〜' ,'お腹すいた' , 'いらいら']},
-        {q: 'ねこがしっぽをピーンとしているときの気持ちで間違っているのは？' , c: ['警戒！', 'うれしい' ,'かまって' , 'お腹すいた']},
-    ]);
+        {q: 'ねこがふみふみしているときの気持ちで誤っているのは？' , c: ['むかつくわ〜', 'かまって〜' ,'ねむたい' , '落ち着くわ〜']},
+        {q: 'ねこがすりすりするときの気持ちは？' , c: ['においつけとこ', 'いいにおいで癒される〜','はやくあそぼ〜' ]},
+        {q: 'ねこがしっぽを振るときの気持ちは？' , c: ['むかつくわ〜', '興奮してきた！' ,'ワクワク！' , 'お腹すいた']},
+        {q: 'ねこがゴロンしてお腹を見せるときの気持ちは？' , c: ['こっち見て！', 'つかれた！' ,'お腹すいた！' , 'ねむい！']},
+        {q: 'ねこがゴロンしてお腹を見せるときの気持ちは１？' , c: ['こっち見て！', 'つかれた！' ,'お腹すいた！' , 'ねむい！']},
+        {q: 'ねこがゴロンしてお腹を見せるときの気持ちは２？' , c: ['こっち見て！', 'つかれた！' ,'お腹すいた！' , 'ねむい！']},
+        {q: 'ねこがゴロンしてお腹を見せるときの気持ちは３？' , c: ['こっち見て！', 'つかれた！' ,'お腹すいた！' , 'ねむい！']},
+        {q: 'ねこがゴロンしてお腹を見せるときの気持ちは４？' , c: ['こっち見て！', 'つかれた！' ,'お腹すいた！' , 'ねむい！']},
+        {q: 'ねこがゴロンしてお腹を見せるときの気持ちは５？' , c: ['こっち見て！', 'つかれた！' ,'お腹すいた！' , 'ねむい！']},
+
+    ];
 
     
     let currentNum = 0;
     let isAnswerd; 
-    let score =0;
 
         function shuffle(arr) {
 
@@ -46,12 +43,10 @@
         if(li.textContent === quizSet[currentNum].c[0]) {
             li.classList.add('correct');
             btn.classList.remove('disabled');
-            score = score + 10;
         } else {
             li.classList.add('wrong');
-            score = score - 2.5;
-
-        }        
+        }
+        
     }
 
     function setQuiz() {
@@ -68,14 +63,17 @@
             li.textContent = choice;
             li.addEventListener('click' , () => {
                 checkAnswer(li);
+
             });
             choices.appendChild(li);
         });
+
 
         if (currentNum === quizSet.length -1) {
         btn.textContent = '成績をみる';
 
     }
+
     }
 
 
@@ -87,28 +85,12 @@ btn.addEventListener('click' , () => {
         return;
     }
     btn.classList.add('disabled');
-
-    if (currentNum === quizSet.length -1) {
-        scoreLabel.textContent = `あなたのねこ理解度は...${score}％`;
-        result.classList.remove('hidden');
-    }else{
-        currentNum++;
-        setQuiz();
-    }
-
-    if(90 <= score && score <= 100){
-        scoreText.textContent =`あなたはねこの良き理解者です。ねこからの信頼も厚いので、今後も良い関係が築けるでしょう。`;
-    }else if(80 <= score && score <= 89){
-        scoreText.textContent =`あなたはねこといい友達です。基本的に害がないいいやつとして見られているでしょう。`;
-    }else if(60 <= score && score <= 79){
-        scoreText.textContent =`あなたはねこをぼちぼち分かっています。しかしねこは気まぐれなので、まだまだ好かれる努力を続けてください。`;
-    }else{
-        scoreText.textContent =`あなたはねこの敵とみなされています。ねこ様に好かれるためには努力が必要です。まずは気持ちを汲み取れるよう観察からはじめてみましょう。`;
-    }
+    currentNum++;
+    setQuiz();
 
 });
 
-
+let score =0 ;
 
 
 
