1d52cea56dbc2881628beb3d45c1a19f3f7681e9
suzuka92 <suzukasato.job@gmail.com>
2020-03-14 23:45:49 +0900

    Bootstrap宣言削除

--- cafe-bell/src/index.js
+++ cafe-bell/src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
 //↓react-bootstrapをインストールした記述
 import 'bootstrap/dist/css/bootstrap.min.css';
 
